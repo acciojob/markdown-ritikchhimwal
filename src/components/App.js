3f7df238829a9f4be519985d8f8ab7fc8ac3848e
ritikchhimwal <ritikchhimwal822@gmail.com>
2024-12-28 20:11:05 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,20 @@
-
-import React, { useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-// import '../styles/App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-    const [markdown, setMarkdown] = useState("")
-    const [loading, setLoading] = useState(true)
- 
-    const hancleChange = (e) => {
-        setMarkdown(e.target.value)
-    }
- 
-    return (
-        <div className='app'>
-            <textarea className="textarea" onChange={hancleChange}></textarea>
-            <ReactMarkdown className="preview" children={markdown} />
-        </div>
-    )
+  const [markdown, setMarkdown] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const hancleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  return (
+    <div className="app">
+      <textarea className="textarea" onChange={hancleChange}></textarea>
+      <ReactMarkdown className="preview" children={markdown} />
+    </div>
+  );
 }
 
-
 export default App;
-
-{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
